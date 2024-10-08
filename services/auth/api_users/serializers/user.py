373b79736f5132ed0295e61849c237d3bfb6a865
vinhnado <vinh.nd0231@gmail.com>
2024-10-08 20:18:58 +0700

    update new base

---
+++ services/auth/api_users/serializers/user.py
@@ -0,0 +1,52 @@
+from rest_framework import generics, permissions, serializers
+
+from api_users.models.user import User
+from api_users.models.role import Role
+from django.contrib.auth.hashers import make_password
+
+
+# first we define the serializers
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "email", "first_name", "last_name"]
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "email", "password", "first_name", "last_name"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        try:
+            user = User.objects.create(**validated_data)
+            role, _ = Role.objects.get_or_create(
+                name="User",
+                description="User role",
+                scope="__all__"
+            )
+            user.roles.add(role)
+            user.set_password(validated_data["password"])
+            user.is_superuser = False
+            user.save()
+            return user
+        except Exception as e:
+            self.delete(user)  # Call delete method to rollback the user creation
+            raise e  # Re-raise the exception to propagate it further
+
+    def delete(self, instance):
+        if instance and instance.pk:
+            instance.delete()
+
+    
+class ShortUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "email",
+            "first_name",
+            "last_name",
+        ]
+        depth = 1
+
---
+++ services/auth/base/middleware/exception_handling.py
@@ -0,0 +1,23 @@
+import logging
+from django.http import JsonResponse
+from base.middleware.exception import CustomMiddlewareException
+
+
+class ExceptionHandlingMiddleware:
+    """Handle uncaught exceptions instead of raising a 500.
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        try:
+            response = self.get_response(request)
+        except Exception as exception:
+            return self.process_exception(request, exception)
+        return response
+
+    def process_exception(self, request, exception):
+        logging.error(f"An error occurred: {exception}", exc_info=True)
+        if isinstance(exception, CustomMiddlewareException):
+            return JsonResponse({'error': str(exception)})
+        return JsonResponse({'error': 'An unexpected error occurred.'}, status=500)
