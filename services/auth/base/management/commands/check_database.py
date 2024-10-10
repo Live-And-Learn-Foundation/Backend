76a04db033b12883ef010da04d7db9b4dcf48bc8
vinhnado <vinh.nd0231@gmail.com>
2024-10-10 18:24:49 +0700

    update

--- services/auth/base/management/commands/check_database.py
+++ services/auth/base/management/commands/check_database.py
@@ -11,8 +11,7 @@ class Command(BaseCommand):
         db_name = os.getenv('DB_NAME')
         db_user = os.getenv('DB_USER')
         db_password = os.getenv('DB_PASSWORD')
-        # db_host = os.getenv('DB_HOST')
-        db_host = 'localhost'
+        db_host = os.getenv('DB_HOST')
         db_port = os.getenv('DB_PORT', 3306)
 
         try:
